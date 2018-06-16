f2aafcefa5010abe64ed652df67728ef0af077db
AtsushiUMAKATSU <mamamatz@gmail.com>
2018-06-16 23:24:45 +0900

    first commit

---
+++ lambda/custom/index.js
@@ -0,0 +1,225 @@
+'use strict';
+const Alexa = require("alexa-sdk");
+
+// For detailed tutorial on how to making a Alexa skill,
+// please visit us at http://alexa.design/build
+
+const STATE = require("./state");
+const MESSAGE = require("./message");
+
+exports.handler = function(event, context) {
+    console.log(JSON.stringify(event, ' ', 4));
+
+    let alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(
+        handlers,
+        ActionHandler,
+        CountHandler,
+        CheerHandler,
+        ChallengeHandler,
+        BuaCountHandler
+        );
+    alexa.appId = "amzn1.ask.skill.cac74044-4888-4d5d-875c-23047d9357f9";
+    alexa.execute();
+};
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('MuscleIntent');
+    },
+    'MuscleIntent': function () {
+        this.handler.state = STATE.LOGIN;
+        this.response.speak(MESSAGE.login.speak)
+            .listen(MESSAGE.login.reprompt);
+        this.emit(':responseReady');
+    },
+    'SessionEndedRequest' : function() {
+        console.log('Session ended with reason: ' + this.event.request.reason);
+    },
+    'AMAZON.StopIntent' : function() {
+        this.response.speak(MESSAGE.exit.speak);
+        this.emit(':responseReady');
+    },
+    'AMAZON.HelpIntent' : function() {
+        this.response.speak(MESSAGE.help.speak)
+            .listen(MESSAGE.help.reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent' : function() {
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled' : function() {
+        this.response.speak(MESSAGE.error.speak)
+            .listen(MESSAGE.error.reprompt);
+    }
+};
+
+const ActionHandler = Alexa.CreateStateHandler(STATE.LOGIN, {
+    'MuscleIntent': function() {
+        this.emitWithState('AMAZON.YesIntent');
+    },
+    'AMAZON.YesIntent': function () {
+            this.handler.state = STATE.COUNT;
+        this.response.speak(MESSAGE.count.speak)
+            .listen(MESSAGE.count.reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.NoIntent': function () {
+        this.handler.state = STATE.CHALLENGE;
+        this.response.speak(MESSAGE.challenge.speak)
+            .listen(MESSAGE.challenge.reprompt);
+        this.emit(':responseReady');
+    },
+    'SessionEndedRequest' : function() {
+        console.log('Session ended with reason: ' + this.event.request.reason);
+    },
+    'AMAZON.StopIntent' : function() {
+        this.response.speak(MESSAGE.exit.speak);
+        this.emit(':responseReady');
+    },
+    'AMAZON.HelpIntent' : function() {
+        this.response.speak(MESSAGE.help.speak)
+            .listen(MESSAGE.help.reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent' : function() {
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled' : function() {
+        this.response.speak(MESSAGE.error.speak)
+            .listen(MESSAGE.error.reprompt);
+    }
+});
+
+const CountHandler = Alexa.CreateStateHandler(STATE.COUNT, {
+    'MuscleIntent': function () {
+        this.handler.state = STATE.BUA_COUNT;
+        this.response.speak(MESSAGE.bua_count.speak)
+            .listen(MESSAGE.bua_count.reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.YesIntent': function () {
+        this.emitWithState('MuscleIntent');
+    },
+    'AMAZON.NoIntent': function () {
+        this.handler.state = STATE.CHEER;
+        this.response.speak(MESSAGE.cheer.speak)
+            .listen(MESSAGE.cheer.reprompt);
+        this.emit(':responseReady');
+    },
+    'SessionEndedRequest' : function() {
+        console.log('Session ended with reason: ' + this.event.request.reason);
+    },
+    'AMAZON.StopIntent' : function() {
+        this.response.speak(MESSAGE.exit.speak);
+        this.emit(':responseReady');
+    },
+    'AMAZON.HelpIntent' : function() {
+        this.response.speak(MESSAGE.help.speak)
+            .listen(MESSAGE.help.reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent' : function() {
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled' : function() {
+        this.response.speak(MESSAGE.error.speak)
+            .listen(MESSAGE.error.reprompt);
+    }
+});
+
+
+const CheerHandler = Alexa.CreateStateHandler(STATE.CHEER, {
+    'AMAZON.YesIntent': function () {
+        this.handler.state = STATE.BUA_COUNT;
+        this.response.speak(MESSAGE.bua_count.speak)
+            .listen(MESSAGE.bua_count.reprompt);
+        this.emit(':responseReady');
+        },
+    'AMAZON.NoIntent': function () {
+        this.emit('AMAZON.StopIntent');
+    },
+    'SessionEndedRequest' : function() {
+        console.log('Session ended with reason: ' + this.event.request.reason);
+    },
+    'AMAZON.StopIntent' : function() {
+        this.response.speak(MESSAGE.exit.speak);
+        this.emit(':responseReady');
+    },
+    'AMAZON.HelpIntent' : function() {
+        this.response.speak(MESSAGE.help.speak)
+            .listen(MESSAGE.help.reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent' : function() {
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled' : function() {
+        this.response.speak(MESSAGE.error.speak)
+            .listen(MESSAGE.error.reprompt);
+    }
+});
+
+const ChallengeHandler = Alexa.CreateStateHandler(STATE.CHALLENGE, {
+    'MuscleIntent': function () {
+      this.emitWithState('AMAZON.YesIntent');
+    },
+    'AMAZON.YesIntent': function () {
+        this.handler.state = STATE.COUNT;
+        this.response.speak(MESSAGE.count.speak)
+            .listen(MESSAGE.count.reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.NoIntent': function () {
+        this.emit('AMAZON.StopIntent');
+    },
+    'SessionEndedRequest' : function() {
+        console.log('Session ended with reason: ' + this.event.request.reason);
+    },
+    'AMAZON.StopIntent' : function() {
+        this.response.speak(MESSAGE.exit.speak);
+        this.emit(':responseReady');
+    },
+    'AMAZON.HelpIntent' : function() {
+        this.emit('AMAZON.StopIntent');
+    },
+    'AMAZON.CancelIntent' : function() {
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled' : function() {
+        this.response.speak(MESSAGE.error.speak)
+            .listen(MESSAGE.error.reprompt);
+    }
+});
+
+const BuaCountHandler = Alexa.CreateStateHandler(STATE.BUA_COUNT, {
+    'MuscleIntent': function () {
+        this.emitWithState('AMAZON.YesIntent');
+    },
+    'AMAZON.YesIntent': function () {
+        this.handler.state = STATE.COUNT;
+        this.response.speak(MESSAGE.count.speak)
+            .listen(MESSAGE.count.reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.NoIntent': function () {
+        this.emit('AMAZON.StopIntent');
+    },
+    'SessionEndedRequest' : function() {
+        console.log('Session ended with reason: ' + this.event.request.reason);
+    },
+    'AMAZON.StopIntent' : function() {
+        this.response.speak(MESSAGE.exit.speak);
+        this.emit(':responseReady');
+    },
+    'AMAZON.HelpIntent' : function() {
+        this.emit('AMAZON.StopIntent');
+    },
+    'AMAZON.CancelIntent' : function() {
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled' : function() {
+        this.response.speak(MESSAGE.error.speak)
+            .listen(MESSAGE.error.reprompt);
+    }
+});
---
+++ lambda/custom/message.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "login": {
+        "speak": "いい調子だね。まだいけそう？",
+        "reprompt": "まだいけそう？"
+    },
+    "count": {
+        // アブアブ
+        "speak": "じゃあ、あと5回カウントするよ。<audio src='https://s3-ap-southeast-2.amazonaws.com/objc-spajam/abu_voice001.mp3' />。まだいけそう？",
+        "reprompt": "まだいけそう？"
+    },
+    "bua_count": {
+        // ブアブア
+        "speak": "じゃああと5回！<audio src='https://s3-ap-southeast-2.amazonaws.com/objc-spajam/abu_voice003.mp3' />。だいじょうぶ？",
+        "reprompt": "まだいけそう？"
+    },
+    "challenge": {
+        // 目を閉じて。筋肉の声を聞いて
+        "speak": "<audio src='https://s3-ap-southeast-2.amazonaws.com/objc-spajam/ouen_voice003.mp3' />。まだいけるよね？",
+        "reprompt": "もう無理かな？"
+    },
+    "cheer": {
+        // 負けそうでも限界まで行っちゃえ
+      "speak": "そうですか。<audio src='https://s3-ap-southeast-2.amazonaws.com/objc-spajam/ouen_voice001.mp3' />。もう無理かな？",
+      "reprompt": "まだいけそう？"
+    },
+    "error": {
+      "speak": "君に筋肉の声が聞こえなかったよ。まだいけそう？",
+      "reprompt": "まだいけそう？"
+    },
+    "exit": {
+        "speak": "よく頑張ったね。ゆっくり休んでね。"
+    },
+    "help": {
+        // 体がなまってるみんな。運動の時間だよ。
+        "speak": "<audio src='https://s3-ap-southeast-2.amazonaws.com/objc-spajam/ouen_voice005.mp3' />。腕立てしますよね？",
+        "reprompt": "腕立てしますよね？"
+    }
+};
